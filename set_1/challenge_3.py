eb70d1f08d1908e7072d0fd5bd9a3afbf1a17094
Context <context@base-7r5.contextis-testing.com>
2017-12-03 17:45:46 +0000

    working on challenge 3, moved to python 3

--- set_1/challenge_3.py
+++ set_1/challenge_3.py
@@ -3,22 +3,41 @@
 """http://cryptopals.com/sets/1/challenges/3"""
 
 from hex_to_base64 import string_to_bytearray
+from binascii import unhexlify
 
 def xor_single(sample, xor_char):
-    in_bytes = string_to_bytearray(sample)
+    in_bytes = unhexlify(sample)
     output = bytearray()
     for i in in_bytes:
         output.append(i^xor_char)
     print(output.__repr__())
-
+    return output
 
 def charater_frequency(bytearray_sample):
-    start = bytes("61")
-    end = bytes("7a")
+    start_upper = ord("A")
+    end_upper = ord("Z")
 
-    
+    start_lower = ord("a")
+    end_lower = ord("z")
+
+
 
 if __name__ == '__main__':
+
     a = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    for i in range(0, 255):
-        xor_single(a, i)
+    b = xor_single(a, 56)
+
+    char_freq = {}
+    start_upper = ord("A")
+    end_upper = ord("Z")
+
+    for i in b:
+        print(i)
+        if start_upper <= i <= end_upper:
+            dict_value = chr(i).lower()
+            if dict_value in char_freq:
+                char_freq[dict_value] += 1
+            else:
+                char_freq[dict_value] = 1
+
+    print(char_freq)
