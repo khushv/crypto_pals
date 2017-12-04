d3ae97e96afc7108340e624f1b6593f0e33447ff
Context <context@base-7r5.contextis-testing.com>
2017-12-04 16:29:22 +0000

    finished chall_3

--- set_1/challenge_3.py
+++ set_1/challenge_3.py
@@ -6,38 +6,86 @@ from hex_to_base64 import string_to_bytearray
 from binascii import unhexlify
 
 def xor_single(sample, xor_char):
-    in_bytes = unhexlify(sample)
-    output = bytearray()
-    for i in in_bytes:
-        output.append(i^xor_char)
-    print(output.__repr__())
-    return output
+	in_bytes = unhexlify(sample)
+	output = bytearray()
+	for i in in_bytes:
+		output.append(i^xor_char)
+	#print(output.__repr__())
+	return output
 
-def charater_frequency(bytearray_sample):
-    start_upper = ord("A")
-    end_upper = ord("Z")
+def character_frequency(bytearray_sample):
+	start_upper = ord("A")
+	end_upper = ord("Z")
 
-    start_lower = ord("a")
-    end_lower = ord("z")
+	start_lower = ord("a")
+	end_lower = ord("z")
+
+	char_freq_dict = {}
+	for i in bytearray_sample:
+		add_to_dict(start_upper, end_upper, i, char_freq_dict)
+		add_to_dict(start_lower, end_lower, i, char_freq_dict)
+	return char_freq_dict
+
+def add_to_dict(lower, upper, letter, letter_dict):
+	if lower <= letter <= upper:
+		dict_value = chr(letter).lower()
+		# replace below with defaultdict
+		if dict_value in letter_dict:
+			letter_dict[dict_value] += 1
+		else:
+			letter_dict[dict_value] = 1
 
 
+def etaoin_frequency(chars_dict):
+	if not chars_dict:
+		return "10000000"
+	else:
+		total_chars = sum(chars_dict.values())
+		frequency = {}
+		for key, value  in chars_dict.items():
+			frequency[key] = value / total_chars
+
+		x = sorted(k for k,v in frequency.items())
+		x_string = ''.join(x)
+		#blahblah = my_levenshtein("etaoinshrdlucmfgypwbvkxjqz", x_string)
+		#only returning total number of chars, overcomplicating problem
+		return total_chars
+
+
+def my_levenshtein(s1, s2):
+	if len(s1) < len(s2):
+		return my_levenshtein(s2, s1)
+
+	# len(s1) >= len(s2)
+	if len(s2) == 0:
+		return len(s1)
+
+	previous_row = range(len(s2) + 1)
+	for i, c1 in enumerate(s1):
+		current_row = [i + 1]
+		for j, c2 in enumerate(s2):
+			insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+			deletions = current_row[j] + 1	   # than s2
+			substitutions = previous_row[j] + (c1 != c2)
+			current_row.append(min(insertions, deletions, substitutions))
+		previous_row = current_row
+
+	return previous_row[-1]
+
+
+
+def xor_brute(text):
+	list_chars = []
+	for i in range(0, 255):
+		xord = xor_single(text, i)
+		xord_freq = character_frequency(xord)
+		if xord_freq:
+			lev = etaoin_frequency(xord_freq)
+			list_chars.append((lev, i, xord))
+	sorted_char_freq = sorted(list_chars, reverse=True)
+	print(sorted_char_freq[]) # add top 5 for less text
 
 if __name__ == '__main__':
 
-    a = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    b = xor_single(a, 56)
-
-    char_freq = {}
-    start_upper = ord("A")
-    end_upper = ord("Z")
-
-    for i in b:
-        print(i)
-        if start_upper <= i <= end_upper:
-            dict_value = chr(i).lower()
-            if dict_value in char_freq:
-                char_freq[dict_value] += 1
-            else:
-                char_freq[dict_value] = 1
-
-    print(char_freq)
+	a = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	xor_brute(a)
--- set_1/hex_to_base64.py
+++ set_1/hex_to_base64.py
@@ -1,19 +1,24 @@
 import base64
 
 def convert_hex_to_base64(sample):
-    converted_bytes = string_to_bytearray(sample)
-    return bytearray_to_base64(converted_bytes)
+	converted_bytes = string_to_bytearray(sample)
+	return bytearray_to_base64(converted_bytes)
 
 
 def string_to_bytearray(sample):
-    return bytearray.fromhex(sample)
+	return bytearray.fromhex(sample)
 
 def bytearray_to_base64(sample):
-    return base64.b64encode(sample)
+	return base64.b64encode(sample)
 
 
 if __name__ == '__main__':
-    sample = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f"
-    "69736f6e6f7573206d757368726f6f6d"
+	sample = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f"
+	"69736f6e6f7573206d757368726f6f6d"
 
-    print(convert_hex_to_base64(sample))
+	print(convert_hex_to_base64(sample))
+
+
+
+
+#
