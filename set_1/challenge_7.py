7ec035a7d54b9fd32e50b1047b91d99f31240803
Context <context@base-7r5.contextis-testing.com>
2018-01-15 16:46:46 +0000

    added module init files, making start on ecb implementation

--- set_1/challenge_7.py
+++ set_1/challenge_7.py
@@ -2,8 +2,7 @@ import Cryptodome
 from Cryptodome.Cipher import AES
 from challenge_6 import base64_file_to_bytes
 
-def decrypt_base64_AES(file, key):
-	cipher_bytes = base64_file_to_bytes(file)
+def decrypt_AES_ECB_bytes(cipher_bytes, key):
 	key_bytes = key.encode('ascii')
 	cipher_obj = AES.new(key_bytes, AES.MODE_ECB)
 	decrypted_bytes = cipher_obj.decrypt(cipher_bytes)
@@ -11,5 +10,6 @@ def decrypt_base64_AES(file, key):
 	
 
 if __name__ == '__main__':
-	x = decrypt_base64_AES("7.txt", "YELLOW SUBMARINE")
+	cipher_text = base64_file_to_bytes("7.txt")
+	x = decrypt_AES_ECB_bytes(cipher_text, "YELLOW SUBMARINE")
 	print(x)
--- set_2/challenge_9.py
+++ set_2/challenge_9.py
@@ -4,11 +4,11 @@ def pad(sample, block_size):
 	sample_size = len(sample)
 	while sample_size > block_size:
 		sample_size -= block_size
-	sample += b"\x04" * (block_size - sample_size)
+	sample = sample.encode('ascii') + b"\x04" * (block_size - sample_size)
 	return sample
 
 if __name__ == '__main__':
-	a = "abcdefghiklmnop".encode('ascii')
-	pad(a, 4)
+	a = "YELLOW SUBMARINE" #.encode('ascii')
+	print(pad(a, 20))
 	
 	
