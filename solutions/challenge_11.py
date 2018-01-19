6ac274ed1a0c10a87cf4b9f267f4515747ca0be9
Context <context@base-7r5.contextis-testing.com>
2018-01-19 13:27:46 +0000

    on to challenge 12

--- solutions/challenge_11.py
+++ solutions/challenge_11.py
@@ -10,41 +10,45 @@ def detect_ecb(plaintext, block_size):
 	#print("Number of plaintext: ", len(plaintext))
 	#print("Set number: ", len(set(plaintext)))
 	if len(set(plaintext)) != len(plaintext):
-		#probs ecb
-		return True
+		print("ECB")
+		#return True
 	else:
-		return False
-
+		print("CBC")
+		#return False
+	
+	
 def encryption_oracle(plain_bytes, block_size):
 	key = urandom(block_size)
 	append = urandom(randint(5,10))
 	preppend = urandom(randint(5,10))
-	plain_bytes = append + plain_bytes + preppend
+	#plain_bytes = append + plain_bytes + preppend
 	plain_bytes = pad(plain_bytes, block_size)
 	print("Plainbyte length: ", len(plain_bytes))
 	if randint(0,1):
 		encrypted = encrypt_AES_ECB(plain_bytes, key)
 		#print("Making ECB")
-		return (0, encrypted)
+		return encrypted
 	else:
 		iv = urandom(block_size)
 		encrypted = encrypt_AES_CBC(plain_bytes, iv, key, block_size)
 		#print("Making CBC")
-		return (1, encrypted)
+		return encrypted
 
 
 
 if __name__ == '__main__':
 	BLOCK_SIZE = 16
-	plaintext = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".encode('ascii')
+	my_input = "A" * 33
+	plaintext = my_input.encode('ascii')
 	print(len(plaintext))
-	for i in range(0, 10):
+	while 1:
 		encrypted = encryption_oracle(plaintext, BLOCK_SIZE)
-		if encrypted[0] == 0 and detect_ecb(encrypted[1], BLOCK_SIZE):
+		detect_ecb(encrypted, BLOCK_SIZE)
+	"""	if encrypted[0] == 0 and detect_ecb(encrypted[1], BLOCK_SIZE):
 			print("True")
 		elif encrypted[0] == 1 and not detect_ecb(encrypted[1], BLOCK_SIZE):
 			print("True CBC")
 		else:
 			print("FALSE")
-		
+	"""
 
---
+++ solutions/challenge_12.py
@@ -0,0 +1,17 @@
+from os import urandom
+from challenge_11 import detect_ecb
+
+to_decrypt = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg"
+"aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq"
+"dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg"
+"YnkK"
+
+random_stable_key = 
+
+def ecb_oracle(plain_bytes, block_size):
+	
+
+if __name__ == '__main__':
+	BLOCK_SIZE = 16
+	my_input = "A" * 33
+	print(to_decrypt)
