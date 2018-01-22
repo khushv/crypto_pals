abd926fc148ebf02d488377224cef43058f3201c
Context <context@base-7r5.contextis-testing.com>
2018-01-22 09:38:00 +0000

    adding notes

--- solutions/challenge_12.py
+++ solutions/challenge_12.py
@@ -22,16 +22,19 @@ def ecb_oracle(plain_bytes, key, block_size):
 	secret_string = b64decode(fake_string)
 	plain_bytes += secret_string
 	plain_bytes = pad(plain_bytes, block_size)
+	print("Length of plaintext: ", len(plain_bytes))
 	return encrypt_AES_ECB(plain_bytes, key)
 
 if __name__ == '__main__':
 	#setup
 	BLOCK_SIZE = 16
 	key_size = 16 # 24 or 32
-	my_input = "A" * 33
+	my_input = "A" * 32
 	key = sys.argv[1].encode('utf-8', 'surrogateescape')
 	encrypted_string = ecb_oracle(my_input, key, BLOCK_SIZE)
-	
+	print("Length of encrypted: ", len(encrypted_string))
+	value = detect_ecb(encrypted_string, BLOCK_SIZE)
+	print("ECB mode detected: ", value)
 
 
 """
@@ -39,4 +42,22 @@ run in bash first:
 key=`dd if=/dev/urandom bs=16 count=1`
 python challenge_12.py key
 dont forget to change key size
+
+
+Feed identical bytes of your-string to the function 1 at a time --- start with 1 byte ("A"), then "AA", then "AAA" and so on. Discover the block size of the cipher. You know it, but do this step anyway.
+
+Length of block size is 16
+
+
+Detect that the function is using ECB. You already know, but do this step anyways.
+
+Oracle function detects duplicate blocks. Is ECB!
+
+
+Knowing the block size, craft an input block that is exactly 1 byte short (for instance, if the block size is 8 bytes, make "AAAAAAA"). Think about what the oracle function is going to put in that last byte position.
+
+
+Make a dictionary of every possible last byte by feeding different strings to the oracle; for instance, "AAAAAAAA", "AAAAAAAB", "AAAAAAAC", remembering the first block of each invocation.
+Match the output of the one-byte-short input to one of the entries in your dictionary. You've now discovered the first byte of unknown-string.
+Repeat for the next byte.
 """
