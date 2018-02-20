9096ce049a3e5b56b96ddc1e541aa82f8f331b0e
Context <context@base-7r5.contextis-testing.com>
2018-02-20 12:29:47 +0000

    implemented challenge 15

--- solutions/challenge_13.py
+++ solutions/challenge_13.py
@@ -1,5 +1,8 @@
 import json
+import os
 from random import randrange
+from challenge_9 import pad
+from challenge_10 import encrypt_AES_ECB
 
 def json_parse(cookie_str):
 	cookie_obj = {}
@@ -21,5 +24,19 @@ def encode_user_profile(email_address, role="user"):
 	cookie_str += "&role=" + role.replace('&', '').replace('=', '')
 	return cookie_str
 
+
+def encrypt_profile(encoded_str, key):
+	encoded_str = encoded_str.encode('ascii')
+	encoded_str = pad(encoded.encode('ascii'), BLOCK_SIZE)
+	return encrypt_AES_ECB(encoded_str, key)
+	
+
 if __name__ == '__main__':
-	print(encode_user_profile("bob@bob.com", "user"))
+	BLOCK_SIZE = 16
+	key = os.urandom(BLOCK_SIZE)
+	encoded = encode_user_profile("bob@bob.com", "user")
+	x = encrypt_profile(encoded, key)
+	print(x)
+	
+	
+	
---
+++ solutions/challenge_15.py
@@ -0,0 +1,48 @@
+
+
+def pkcs7_pad_validate(plaintext):
+	last_letter = plaintext[-1].encode('ascii')
+	ll = int.from_bytes(last_letter, byteorder='big')
+	buffer = plaintext[-ll:].encode('ascii')
+	for char in buffer:
+		#print("Compare", char, " with ", last_letter)
+		if char != ll:
+			raise Exception("Invalid padding")
+	return True
+	
+
+
+if __name__ == '__main__':
+	a = "ICE ICE BABY\x04\x04\x04\x04"
+	print(pkcs7_pad_validate(a))
+
+	b = "ICE ICE BABY\x05\x05\x05\x05"
+	print(pkcs7_pad_validate(b))
+
+	c = "ICE ICE BABY\x01\x02\x03\x04"
+	print(pkcs7_pad_validate(c))
+
+
+
+
+
+"""
+PKCS#7 padding validation
+Write a function that takes a plaintext, determines if it has valid PKCS#7 padding, and strips the padding off.
+
+The string:
+
+"ICE ICE BABY\x04\x04\x04\x04"
+... has valid padding, and produces the result "ICE ICE BABY".
+
+The string:
+
+"ICE ICE BABY\x05\x05\x05\x05"
+... does not have valid padding, nor does:
+
+"ICE ICE BABY\x01\x02\x03\x04"
+If you are writing in a language with exceptions, like Python or Ruby, make your function throw an exception on bad padding.
+
+Crypto nerds know where we're going with this. Bear with us.
+
+"""
