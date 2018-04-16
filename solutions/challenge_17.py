e469ca6f4433b16677cad8f482ac131c3b943cea
Context <context@build7R7.contextis-testing.com>
2018-04-16 17:00:26 +0100

    still on padding oracle

--- solutions/challenge_17.py
+++ solutions/challenge_17.py
@@ -6,7 +6,31 @@ from challenge_6 import keysize_blocks
 from challenge_9 import pad
 from challenge_15 import pkcs7_pad_validate_bytes
 
-def encrypt_func(encryption_key, iv):
+def encrypt_func(plaintext, encryption_key, iv):
+    #print("Character should be: ", plaintext[-1])
+    cipher_obj = AES.new(encryption_key, AES.MODE_CBC, iv)
+    encrypted_bytes = cipher_obj.encrypt(plaintext)  
+    return encrypted_bytes
+    
+
+def decrypt_func(cipher_text, key, iv):
+    cipher_obj = AES.new(key, AES.MODE_CBC, iv)
+    decrypted_bytes = cipher_obj.decrypt(cipher_text)
+    #print("******")    
+    #print(decrypted_bytes)
+    if pkcs7_pad_validate_bytes(decrypted_bytes):
+        return True
+    else:
+        return False
+
+
+ 
+
+if __name__ == '__main__':
+    BLOCK_SIZE = 16
+    key = bytes(16) #urandom(BLOCK_SIZE)
+    iv = bytes(16) #urandom(BLOCK_SIZE)
+
     sample = """MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=
     MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=
     MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==
@@ -19,37 +43,22 @@ def encrypt_func(encryption_key, iv):
     MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93"""
     clean_sample = sample.split("\n")
     clean_sample = [string.strip(" ") for string in clean_sample]
-    plaintext = pad(choice(clean_sample).encode('ascii'), BLOCK_SIZE)
-    print("Character should be: ", plaintext[-1])
-    cipher_obj = AES.new(encryption_key, AES.MODE_CBC, iv)
-    encrypted_bytes = cipher_obj.encrypt(plaintext)
-    return encrypted_bytes
-    
+    #plaintext = pad(choice(clean_sample).encode('ascii'), BLOCK_SIZE)
+    plaintext = pad(clean_sample[1].encode('ascii'), BLOCK_SIZE)
+    print(plaintext)
 
-def decrypt_func(cipher_text, key, iv):
-    cipher_obj = AES.new(key, AES.MODE_CBC, iv)
-    decrypted_bytes = cipher_obj.decrypt(cipher_text)
-    #print(decrypted_bytes)
-    if pkcs7_pad_validate_bytes(decrypted_bytes):
-        return True
-    else:
-        return False
+    cipher_text = encrypt_func(plaintext, key, iv)
+    print("Cipher text is: ", cipher_text)
+    #print("Length of cipher text is: ", len(cipher_text))
+    decrypted = decrypt_func(cipher_text, key, iv)
+    print("test")
 
 
- 
-
-if __name__ == '__main__':
-    BLOCK_SIZE = 16
-    key = urandom(BLOCK_SIZE)
-    iv = urandom(BLOCK_SIZE)
-    cipher_text = encrypt_func(key, iv)
-    decrypt_func(cipher_text, key, iv)
-    
     blocks = keysize_blocks(BLOCK_SIZE, cipher_text)
     second_last = bytearray(blocks[-2])
     last_char = second_last[-1]
-    print("Last char is: ", last_char)    
-    for i in range(0, 254):
+    print("last character is: ", last_char)
+    for i in range(0, 255):
         #import pdb; pdb.set_trace()
         mod_second_last = second_last[0:-1]
         mod_second_last.append(i)
@@ -57,9 +66,12 @@ if __name__ == '__main__':
         mod_blocks[-2] = mod_second_last
         a = bytes().join(mod_blocks)
         #
-        if decrypt_func(a, key, iv):
-            print("character: ", i)
-            print(mod_second_last)
+        if decrypt_func(a, key, iv) and i != last_char:
+            print("Character in dec: ", i, " and in hex: ", hex(i), " and in ascii: ", chr(i))
+
+
+
+
 
 
 
